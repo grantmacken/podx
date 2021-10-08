888851ad816509251109b5042502be6118ac1c45
grantmacken <grantmacken@gmail.com>
2021-10-09 11:28:39 +1300

    creat selfsign certs

--- inc/certs.mk
+++ inc/certs.mk
@@ -1,27 +1,36 @@
 ##############
 # self signed certs for example.com
 ##############
-.PHONY: certs-self-sign
-certs-self-sign: src/proxy/certs/$(DOMAIN).crt \
-	src/proxy/certs/dhparam.pem \
-	src/proxy/conf/self-signed.conf
 
-certs-pem: src/proxy/certs/$(DOMAIN).pem # or must be running
+certs-volume: deploy/certs-volume.tar
+certs-conf:  src/proxy/conf/self_signed.conf
+cacert: src/proxy/certs/$(DOMAIN).pem # or must be running
+
+certs-volume-check: 
+		@podman run --rm  --mount $(MountCerts) --entrypoint '["sh", "-c"]' $(ALPINE) \
+			'ls ../certs'
+
+deploy/certs-volume.tar: src/proxy/certs/example.com.crt src/proxy/certs/dhparam.pem
+		@podman run --rm  --interactive  --mount $(MountCerts) --entrypoint '["sh", "-c"]'  $(ALPINE) \
+			'tar -czf - /opt/proxy/certs' 2>/dev/null > $@
 
 .PHONY: certs-clean
 certs-clean: 
-	@rm -fv src/proxy/certs/* src/proxy/conf/self-signed.conf
-	@podman run --interactive --rm  --mount $(MountCerts) --entrypoint "sh" $(OPENRESTY) \
-		-c 'rm -fv ./certs/*'
-	@podman run --interactive --rm  --mount $(MountProxyConf) --entrypoint "sh" $(OPENRESTY) \
-		-c 'rm -fv ./conf/self-signed.conf'
+	@rm -fv deploy/certs-volume.tar
+	@rm -fv src/proxy/certs/* src/proxy/conf/self_signed.conf
+	@podman run --rm  --mount $(MountCerts) --entrypoint '["sh", "-c"]' $(ALPINE) 'rm -fRv /opt/proxy/certs/*'
+	@podman run --rm  --mount $(MountProxyConf) --entrypoint '["sh", "-c"]' $(ALPINE) 'rm -fv /opt/proxy/conf/self_signed.conf'
+	@podman run --rm --workdir /opt/proxy $(ALPINE) ls certs
+	@podman run --rm --workdir /opt/proxy $(ALPINE) ls conf
 
 src/proxy/certs/example.com.key:
 	@[ -d $(dir $@) ] || mkdir -p $(dir $@)
 	@echo '##[ $(notdir $@) ]##'
 	 @openssl genrsa -out $@ 2048
-	@cat $@ | podman run --interactive --rm  --mount $(MountCerts) --entrypoint "sh" $(OPENRESTY) \
-		-c 'cat - > /opt/proxy/certs/$(notdir $@)'
+	@cat $@ | \
+		podman run --rm --interactive --mount $(MountCerts) --entrypoint '["sh", "-c"]' $(ALPINE) \
+		'cat - > /opt/proxy/certs/$(notdir $@)'
+	@podman run --rm  --mount $(MountCerts) $(ALPINE) ls -al /opt/proxy/certs
 
 src/proxy/certs/example.com.csr: src/proxy/certs/example.com.key
 	@[ -d $(dir $@) ] || mkdir -p $(dir $@)
@@ -35,28 +44,28 @@ src/proxy/certs/example.com.crt: src/proxy/certs/example.com.csr
 	@[ -d $(dir $@) ] || mkdir -p $(dir $@)
 	@echo '##[ $(notdir $@) ]##'
 	@openssl x509 -req -days 365 -in $< -signkey src/proxy/certs/example.com.key -out $@ -sha512
-	@cat $@ | podman run --interactive --rm  --mount $(MountCerts) --entrypoint "sh" $(OPENRESTY) \
-		-c 'cat - > /opt/proxy/certs/$(notdir $@)'
+	@cat $@ | \
+		podman run --rm --interactive  --mount $(MountCerts) --entrypoint '["sh", "-c"]' $(ALPINE) \
+		'cat - > /opt/proxy/certs/$(notdir $@)'
+	@podman run --rm  --mount $(MountCerts) $(ALPINE) ls -al /opt/proxy/certs
 
 src/proxy/certs/dhparam.pem:
 	@[ -d $(dir $@) ] || mkdir -p $(dir $@)
 	@echo '##[ $(notdir $@) ]##'
 	@openssl dhparam -out $@ 2048
-	@cat $@ | podman run --interactive --rm  --mount $(MountCerts) --entrypoint "sh" $(OPENRESTY) \
-		-c 'cat - > /opt/proxy/certs/$(notdir $@)'
+	@cat $@ | \
+		podman run --rm --interactive --mount $(MountCerts) --entrypoint '["sh", "-c"]' $(ALPINE) \
+		'cat - > /opt/proxy/certs/$(notdir $@)'
 
-src/proxy/conf/self-signed.conf:
+src/proxy/conf/self_signed.conf:
 	@[ -d $(dir $@) ] || mkdir -p $(dir $@)
 	@echo '## $(notdir $@) ##'
 	@echo "ssl_certificate /opt/proxy/certs/example.com.crt;" > $@
 	@echo "ssl_certificate_key /opt/proxy/certs/example.com.key;" >> $@
 	@echo "ssl_dhparam /opt/proxy/certs/dhparam.pem;" >> $@
-	@cat $@ | podman run --interactive --rm  --mount $(MountProxyConf) --entrypoint "sh" $(OPENRESTY) \
-		-c 'cat - > ./conf/$(notdir $@)'
-		@podman run --interactive --rm  --mount $(MountProxyConf) --entrypoint "sh" $(OPENRESTY) \
-		-c 'ls -al ./conf '
-		@podman run --interactive --rm  --mount $(MountCerts) --entrypoint "sh" $(OPENRESTY) \
-		-c 'ls -al ./certs '
+	@cat $@ | \
+		podman run --rm  --interactive --mount $(MountProxyConf) --entrypoint '["sh", "-c"]' $(ALPINE) \
+		'cat - > /opt/proxy/conf/$(notdir $@)'
 
 src/proxy/certs/$(DOMAIN).pem:
 	@[ -d $(dir $@) ] || mkdir -p $(dir $@)
